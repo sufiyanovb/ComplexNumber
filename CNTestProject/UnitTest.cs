948b8b1b5ab9aa7fe116c5e0a685915f8fd05352
Булат Суфиянов <sufiyanov.b@mail.ru>
2022-04-05 16:02:23 +0300

    add u t

---
+++ CNTestProject/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CNTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
